33e23995777dff220fe03983de2cd35800d86402
Matthew Emerson <emersonmde@protonmail.com>
2023-06-04 08:50:33 -0400

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub mod bloombox {
+    pub struct BloomBox {
+        // your fields here
+    }
+
+    impl BloomBox {
+        pub fn new() -> Self {
+            // your initialization here
+            BloomBox {
+                // field initialization
+            }
+        }
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+    // use super::*;
+
+    // #[test]
+    // fn test_new() {
+    // }
+// }
